0b44893037ebe0d6e2e5af9f082439ae61b4508f
Alexandru <alexturcu11@gmail.com>
2020-09-12 18:30:27 +0300

    starting the e-commerce website

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-import { CardList } from "./components/card-list/card-list.component.jsx";
 
-class App extends Component {
-  constructor() {
-    super();
+import HomePage from "./homepage.component";
 
-    this.state = {
-      monsters: [],
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <CardList monsters={this.state.monsters} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
